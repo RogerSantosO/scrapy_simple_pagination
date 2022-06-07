87846efb62ff76e94d7c8b579fe95ded2882af5f
roger <roger.informe@gmail.com>
2022-06-07 13:23:15 -0300

    atualizacao

--- tinydeal/tinydeal/settings.py
+++ tinydeal/tinydeal/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'tinydeal.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+#USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- tinydeal/tinydeal/spiders/tdeal.py
+++ tinydeal/tinydeal/spiders/tdeal.py
@@ -4,7 +4,7 @@ import scrapy
 class TdealSpider(scrapy.Spider):
     name = 'tdeal'
     allowed_domains = ['web.archive.org']
-    start_urls = ['https://web.archive.org/web/20190225123327/http://www.tinydeal.com/specials.html']
+    start_urls = ['https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html']
 
     def parse(self, response):
         produtos = response.xpath('//div[@class="r_b_c"]/ul/div/li')
@@ -13,7 +13,7 @@ class TdealSpider(scrapy.Spider):
             'name':produto.xpath('.//a[2][@class="p_box_title"]/text()').get(),
             'original price':produto.xpath('.//div[2]/span[@class="normalprice fl"]/text()').get(),
             'discounted price':produto.xpath('.//div[2]/span[@class="productSpecialPrice fl"]/text()').get(),
-            'url':produto.xpath('.//a/@href').get()
+            'url': response.urljoin(produto.xpath('.//a/@href').get())
                 }
             
         next_page = response.xpath('//a[@class="nextPage"]/@href').get()
